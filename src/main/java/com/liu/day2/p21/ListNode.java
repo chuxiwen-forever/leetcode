cc6c1ca703a36b866023eef8de25d3a436c33b60
chuxiwen <1609185446@qq.com>
2023-09-19 15:26:31 +0800

    leetcode p21 拼接两个有序链表

---
+++ src/main/java/com/liu/day2/p21/ListNode.java
@@ -0,0 +1,25 @@
+package com.liu.day2.p21;
+
+/***
+ * @description:
+ * @auther: Liu.
+ * @date: 2023/9/19 14:54
+ * @version 1.0
+ */
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ src/main/java/com/liu/day2/p2560/Solve.java
@@ -0,0 +1,11 @@
+package com.liu.day2.p2560;
+
+/***
+ * @description: leetcode p2560 打家劫舍4
+ * @auther: Liu.
+ * @date: 2023/9/19 14:01
+ * @version 1.0
+ */
+
+public class Solve {
+}
