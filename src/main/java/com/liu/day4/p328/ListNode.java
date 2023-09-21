b4caba150022c385cb08b7cc18561d0f0cbb2b2e
chuxiwen <1609185446@qq.com>
2023-09-21 16:35:07 +0800

    leetcode p328 奇偶链表

---
+++ src/main/java/com/liu/day4/p328/ListNode.java
@@ -0,0 +1,25 @@
+package com.liu.day4.p328;
+
+/***
+ * @description:
+ * @auther: Liu.
+ * @date: 2023/9/21 16:20
+ * @version 1.0
+ */
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
