08e354b34eb4d2961cc7dc4a82507133d12ee5ea
chuxiwen <1609185446@qq.com>
2023-10-23 09:18:49 +0800

    leetcode p2678 老人的数目

---
+++ src/main/java/com/liu/day20/p2678/Solve.java
@@ -0,0 +1,20 @@
+package com.liu.day20.p2678;
+
+/***
+ * @description: <a href="https://leetcode.cn/problems/number-of-senior-citizens/">leetcode p2678 老人的数目</a>。
+ * @auther: chuxiwen
+ * @date: 2023/10/23 09:12
+ * @version 1.0
+ */
+
+public class Solve {
+
+    public int countSeniors(String[] details) {
+        int cnt = 0;
+        for (String detail : details) {
+            int parsed = Integer.parseInt(detail.charAt(11) + "" + detail.charAt(12));
+            if (parsed > 60) cnt++;
+        }
+        return cnt;
+    }
+}
