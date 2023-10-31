4df99a200b7294fc21a4dd0b6803409217dd9662
chuxiwen <1609185446@qq.com>
2023-10-31 16:17:52 +0800

    leetcode p171 Excel 表列序号

---
+++ src/main/java/com/liu/day26/p171/Solve.java
@@ -0,0 +1,21 @@
+package com.liu.day26.p171;
+
+
+/***
+ * @description: <a href="https://leetcode.cn/problems/excel-sheet-column-number/">leetcode p171 Excel 表列序号</a>。
+ * @auther: chuxiwen
+ * @date: 2023/10/31 16:14
+ * @version 1.0
+ */
+
+public class Solve {
+
+    public int titleToNumber(String columnTitle) {
+        int ans = 0;
+        char[] charArray = columnTitle.toCharArray();
+        for (char c : charArray) {
+            ans = ans * 26 + (c - 'A') + 1;
+        }
+        return ans;
+    }
+}
