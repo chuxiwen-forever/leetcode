8147fc5da803bba8f7d35d48c1ce89cc46734d66
chuxiwen <1609185446@qq.com>
2023-11-10 09:56:10 +0800

    leetcode p2300 咒语和药水的成功对数

--- src/main/java/com/liu/week7/day3/p2258/Solve.java
+++ src/main/java/com/liu/week7/day3/p2258/Solve.java
@@ -11,7 +11,6 @@ import java.util.Queue;
  * @version 1.0
  */
 
-
 public class Solve {
 
     private static final int INF = 1000000000;
---
+++ src/main/java/com/liu/week7/day4/p2300/Solve.java
@@ -0,0 +1,31 @@
+package com.liu.week7.day4.p2300;
+
+import java.util.Arrays;
+
+/***
+ * @description: <a href="https://leetcode.cn/problems/successful-pairs-of-spells-and-potions/">leetcode p2300 咒语和药水的成功对数</a>。
+ * @auther: chuxiwen
+ * @date: 2023/11/10 09:41
+ * @version 1.0
+ */
+
+public class Solve {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int n = spells.length;
+        Arrays.sort(potions);
+        int[] res = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            double ans = success * 1.0 / spells[i];
+
+            int left = 0, right = potions.length - 1;
+            while (left < right) {
+                int mid = left + ((right - left) >> 1);
+                if (potions[mid] >= ans) right = mid;
+                else left = mid + 1;
+            }
+            if ((long) potions[left] * spells[i] >= success) res[i] = potions.length - left;
+        }
+        return res;
+    }
+}
