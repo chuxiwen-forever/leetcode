fc6ed741c80e8f06c932627ee3ff5ec04235d757
chuxiwen <1609185446@qq.com>
2023-11-13 11:55:43 +0800

    leetcode p111. 二叉树的最小深度

---
+++ src/main/java/com/liu/week8/day1/p111/Solve.java
@@ -0,0 +1,19 @@
+package com.liu.week8.day1.p111;
+
+/***
+ * @description: <a href="https://leetcode.cn/problems/minimum-depth-of-binary-tree/">leetcode p111. 二叉树的最小深度</a>。
+ * @auther: chuxiwen
+ * @date: 2023/11/13 11:55
+ * @version 1.0
+ */
+
+
+class Solve {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        int m1 = minDepth(root.left);
+        int m2 = minDepth(root.right);
+
+        return root.left == null || root.right == null ? m1 + m2 + 1 : Math.min(m1, m2) + 1;
+    }
+}
---
+++ src/main/java/com/liu/week8/day1/p111/TreeNode.java
@@ -0,0 +1,20 @@
+package com.liu.week8.day1.p111;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
