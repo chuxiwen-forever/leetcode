3b964bb3618056fae244362106215076eb9302c6
chuxiwen <1609185446@qq.com>
2023-11-14 11:36:40 +0800

    leetcode p190. 颠倒二进制位

---
+++ src/main/java/com/liu/week8/day2/p190/Solve.java
@@ -0,0 +1,29 @@
+package com.liu.week8.day2.p190;
+
+
+/***
+ * @description: <a href="https://leetcode.cn/problems/reverse-bits/">leetcode p190. 颠倒二进制位</a>。
+ * @auther: chuxiwen
+ * @date: 2023/11/14 10:59
+ * @version 1.0
+ */
+public class Solve {
+
+    public int reverseBits(int n) {
+        n = ((n & 0xAAAAAAAA) >>> 1) | ((n & 0x55555555) << 1);
+        n = ((n & 0xCCCCCCCC) >>> 2) | ((n & 0x33333333) << 2);
+        n = ((n & 0xF0F0F0F0) >>> 4) | ((n & 0x0F0F0F0F) << 4);
+        n = ((n & 0xFF00FF00) >>> 8) | ((n & 0x00FF00FF) << 8);
+        n = ((n & 0xFFFF0000) >>> 16) | ((n & 0x0000FFFF) << 16);
+        return n;
+    }
+
+    public int reverseBits1(int n) {
+        int rev = 0;
+        for (int i = 0; i < 32 && n != 0; ++i) {
+            rev |= (n & 1) << (31 - i);
+            n >>>= 1;
+        }
+        return rev;
+    }
+}
