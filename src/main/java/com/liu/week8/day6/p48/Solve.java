131997d4fee8a9da7ce605888aff9a1eb9df765b
chuxiwen <1609185446@qq.com>
2023-11-18 10:46:57 +0800

    leetcode p48. 旋转图像

---
+++ src/main/java/com/liu/week8/day6/p48/Solve.java
@@ -0,0 +1,47 @@
+package com.liu.week8.day6.p48;
+
+/***
+ * @description: <a href="https://leetcode.cn/problems/rotate-image/">leetcode p48. 旋转图像</a>。
+ * @auther: chuxiwen
+ * @date: 2023/11/18 09:39
+ * @version 1.0
+ */
+
+
+class Solve {
+
+    // 找规律题
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < (n + 1) / 2; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+
+    // 翻转数组
+    public void rotate1(int[][] matrix) {
+        int n = matrix.length;
+        // 水平翻转
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
+            }
+        }
+        // 主对角线翻转
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}
